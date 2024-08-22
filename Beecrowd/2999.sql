fb19e867cbed23ce3687c14d75ede9a357fde803
isabela-rodrigues <isabelarodriguescos@gmail.com>
2024-08-22 16:23:02 -0300

    Highest Division Salary

--- Beecrowd/2999.sql
+++ Beecrowd/2999.sql
@@ -5,9 +5,14 @@ WITH salario AS (
            emp.lotacao as cod_dep,
            SUM(COALESCE(venc.valor, 0)) as salario_total
     FROM empregado emp
-    LEFT JOIN emp_venc ON emp.matr = emp_venc.matr
-    LEFT JOIN vencimento venc ON emp_venc.cod_venc = venc.cod_venc
-    GROUP BY emp.matr, emp.nome, emp.lotacao, emp.lotacao_div
+    LEFT JOIN emp_venc 
+		ON emp.matr = emp_venc.matr
+    LEFT JOIN vencimento venc 
+		ON emp_venc.cod_venc = venc.cod_venc
+    GROUP BY emp.matr
+		, emp.nome
+		, emp.lotacao
+		, emp.lotacao_div
 ),
 desconto_total AS (
     SELECT salario.matr,
@@ -15,15 +20,21 @@ desconto_total AS (
            salario.lotacao_div,
            SUM(COALESCE(desconto.valor, 0)) AS valor_desconto
     FROM salario
-    LEFT JOIN emp_desc ON emp_desc.matr = salario.matr
-    LEFT JOIN desconto ON emp_desc.cod_desc = desconto.cod_desc
-    GROUP BY salario.matr, salario.nome, salario.lotacao_div, salario.salario_total
+    LEFT JOIN emp_desc 
+		ON emp_desc.matr = salario.matr
+    LEFT JOIN desconto 
+		ON emp_desc.cod_desc = desconto.cod_desc
+    GROUP BY salario.matr
+		, salario.nome
+		, salario.lotacao_div
+		, salario.salario_total
 ),
 geral AS (
     SELECT a.*,
            ROUND(a.salario_total - b.valor_desconto, 2) AS salario_com_desconto
     FROM salario a
-    LEFT JOIN desconto_total b ON a.matr = b.matr
+    LEFT JOIN desconto_total b 
+		ON a.matr = b.matr
 ),
 media AS (
     SELECT cod_dep,
@@ -34,7 +45,8 @@ media AS (
 SELECT geral.nome,
        CAST(geral.salario_com_desconto AS DECIMAL(18,2)) AS salario
 FROM geral
-LEFT JOIN media ON geral.cod_dep = media.cod_dep
+LEFT JOIN media 
+	ON geral.cod_dep = media.cod_dep
 WHERE geral.salario_com_desconto >= 8000
   AND geral.salario_com_desconto > media.media_salarial
 ORDER BY geral.lotacao_div, geral.nome;
