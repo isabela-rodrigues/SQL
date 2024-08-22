bc8d276ac4b3f69747c1423ad4d6320ac37e6a88
isabela-rodrigues <isabelarodriguescos@gmail.com>
2024-08-22 16:00:41 -0300

    Highest Average Salary Divisions

---
+++ Beecrowd/2992.sql
@@ -0,0 +1,60 @@
+WITH salario AS (
+    SELECT emp.matr,
+           emp.lotacao_div,
+           divisao.nome AS nome_div,
+           emp.lotacao AS cod_dep,
+           dep.nome AS nome_dep,
+           SUM(COALESCE(venc.valor, 0)) AS salario_total
+    FROM empregado emp
+    LEFT JOIN emp_venc
+        ON emp.matr = emp_venc.matr
+    LEFT JOIN vencimento venc
+        ON emp_venc.cod_venc = venc.cod_venc
+    LEFT JOIN divisao
+        ON emp.lotacao_div = divisao.cod_divisao
+        AND emp.lotacao = divisao.cod_dep
+    LEFT JOIN departamento dep
+        ON emp.lotacao = dep.cod_dep
+    GROUP BY emp.matr
+        , emp.lotacao
+        , emp.lotacao_div
+        , dep.nome
+        , divisao.nome
+),
+desconto_total AS (
+    SELECT salario.matr,
+           SUM(COALESCE(desconto.valor, 0)) AS valor_desconto
+    FROM salario
+    LEFT JOIN emp_desc 
+        ON emp_desc.matr = salario.matr
+    LEFT JOIN desconto 
+        ON emp_desc.cod_desc = desconto.cod_desc
+    GROUP BY salario.matr
+        , salario.salario_total
+),
+geral AS (
+    SELECT a.matr,
+           a.cod_dep,
+           a.nome_dep,
+           a.lotacao_div,
+           a.nome_div,
+           (a.salario_total - b.valor_desconto) AS salario_com_desconto
+    FROM salario a
+    LEFT JOIN desconto_total b
+        ON a.matr = b.matr
+),
+media_calculada AS (
+    SELECT nome_dep,
+           nome_div,
+           CAST(AVG(salario_com_desconto) AS DECIMAL(18,2)) AS media,
+           ROW_NUMBER() OVER(PARTITION BY nome_dep ORDER BY CAST(AVG(salario_com_desconto) AS DECIMAL(18,2)) DESC) AS id_media
+    FROM geral
+    GROUP BY nome_dep
+        , nome_div
+)
+SELECT nome_dep AS departamento,
+       nome_div AS divisao,
+       media
+FROM media_calculada
+WHERE id_media = 1
+ORDER BY media DESC;
