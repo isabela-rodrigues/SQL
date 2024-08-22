fbf1dc5ee57220a15f3514a0a193d2cf94eae38f
isabela-rodrigues <isabelarodriguescos@gmail.com>
2024-08-22 13:33:35 -0300

    CPF Validations

--- Beecrowd/2625.sql
+++ Beecrowd/2625.sql
@@ -1,2 +1,2 @@
-SELECT CONCAT( SUBSTRING(n.cpf,1,3),'.', SUBSTRING(n.cpf,4,3),'.', SUBSTRING(n.cpf,7,3),'-', SUBSTRING(n.cpf,10,2))
-FROM natural_person n ;
+SELECT CONCAT( SUBSTRING(cpf,1,3),'.', SUBSTRING(cpf,4,3),'.', SUBSTRING(cpf,7,3),'-', SUBSTRING(cpf,10,2))
+FROM natural_person;
